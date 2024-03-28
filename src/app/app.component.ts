b10abb3b83e55449d2989ab19c2d43a40a247517
Seb-Pot-Dev <sebastien.pothee.dev@gmail.com>
2024-03-28 17:56:15 +0100

    Init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { inject } from '@angular/core/testing';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -10,4 +11,6 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'spotify-clone-angular-springboot';
+
+  private faIconLibrary : FaIconLibrary = inject(FaIconLibrary)
 }
---
+++ src/app/shared/font-awesome-icons.ts
@@ -0,0 +1,6 @@
+import {iconDefinition} from "@fortawesome/free-brands-svg-icons";
+import { faCirclePlay } from "@fortawesome/free-regular-svg-icons";
+
+export const fontAwesomeIcons: IconDefinition[] = [
+    faCirclePlay
+]
